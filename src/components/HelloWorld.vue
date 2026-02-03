912f37c9f0e4fba1eb300649da0b1e3c4a3dea1b
Giang Hoang Nguyen <Gng000@uit.no>
2026-02-03 14:14:10 +0100

    added the python server for real this team

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,9 +30,9 @@ const count = ref(0)
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+      >Vue Docs Scaling up Guide</a>.
   </p>
+  
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
